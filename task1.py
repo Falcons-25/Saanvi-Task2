ccccf49e1d74d1a5e23c2444e64446c6ec30a627
SaanviPatil <155999601+SaanviPatil@users.noreply.github.com>
2024-05-19 00:21:55 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,26 @@
+import serial
+
+try:
+    file = open(r"C:\Users\Saanvi\Desktop\Arduino IDE\ultrasonic_test\Ultrasonic.csv", "w", newline="")
+    ser = serial.Serial('COM6', 9600)
+
+    while True:
+        # Read data from serial port
+        data1 = ser.readline().decode().strip()
+        millis, dist = data1.split(',')
+
+        # Print and write data to CSV file
+        print(f"Millis: {millis}, Distance: {dist}\n")
+        file.write(f"Millis: {millis}, Distance: {dist}\n")
+
+
+except ValueError as e:
+    print(e)
+    file.write("COM disconnected \n")
+
+except KeyboardInterrupt as e:
+    print(e)
+    file.write("User intended break \n")
+finally:
+    # Close the CSV file
+    file.close()
